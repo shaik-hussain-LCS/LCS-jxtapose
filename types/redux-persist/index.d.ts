b14989841bbd66cdd136b793de4deb6dd38dc07e
shaik-hussain-LCS <shaik@leafcraftstudios.com>
2026-03-24 13:39:05 +0530

    intial commit

---
+++ types/redux-persist/index.d.ts
@@ -0,0 +1,7 @@
+declare module "redux-persist/lib/integration/react";
+declare module "redux-persist/es/persistStore";
+declare module "redux-persist/es/persistReducer";
+declare module "redux-persist/lib/storage";
+declare namespace React {
+  type StatelessComponent<P> = React.FunctionComponent<P>;
+}
